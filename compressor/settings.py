e059731c442a9578718a3f6fd4cb137f915183c9
Шалейников Евгений <shaleinikove@krasrm.com>
2024-02-01 16:36:47 +0700

    убрал сервис докер в докере из си-сд и вернул движок БД в настройки

--- compressor/settings.py
+++ compressor/settings.py
@@ -57,6 +57,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'compressor.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.'
